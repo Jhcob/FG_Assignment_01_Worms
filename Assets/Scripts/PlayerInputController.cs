3872cdc80facbf55ad8da70b6285dde148b1e3a0
Jorge Cobacho_PC <108235595+Jhcob@users.noreply.github.com>
2022-09-25 20:56:34 +0200

    Movement optimized + jump

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -7,107 +7,66 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputController : MonoBehaviour
 {    
- [SerializeField] [Range(1f, 10f)] private float movementSpeed = 5f;
- [SerializeField] [Range(1f, 10f)] private float sprintSpeed = 10f;
-
-    [SerializeField] [Range(15f, 50f)] private float jumpHeight = 15f;
-    [SerializeField][Range(0.0f, 0.3f)] public float RotationSmoothTime = 0.12f;
-
+    private float movementSpeed; // Adjust at Move function
+    [SerializeField] [Range(1f, 10f)] private float sprintSpeed = 10f;
+    [SerializeField]  private float jumpHeight = 5f;
     [SerializeField] private float gravityValue = -10f;
+    [SerializeField] [Range(0f, 1f)] float rotationSmoothTime = 0.1f;
 
     private Vector2 inputMove;
-    private Vector3 moveDir;
-    private Vector3 Velocity;
-    [SerializeField] [Range(0f, 1f)] float rotationSmoothTime = 0.1f;
-    private float rotationVelocity;
+    //private Vector3 Velocity;
 
-
-    private float targetRotation;
-    private float verticalVelocity;
-    private float _terminalVelocity = 53.0f;
-
-    // timeout deltatime
-    private float _jumpTimeoutDelta;
-    private float _fallTimeoutDelta;
-
-    [Tooltip("Acceleration and deceleration")]
-    public float SpeedChangeRate = 10.0f;
-    
-    private int isMoving;
+    // input button
+    public bool inputJump = false;
     public bool inputSprint = false;
-    public bool Grounded = true;
-    public float GroundedOffset = -0.14f;
-    public float GroundedRadius = 0.28f;
-    public LayerMask GroundLayers;
-    
-    public float JumpTimeout = 0.50f;
-    
-    public Transform cam;
 
-    private CharacterController _characterController;
+    // player
+    private float targetRotation;
+    private float rotationVelocity;
+    private float verticalVelocity;
+
+    
+    public Transform mainCamera;
     //public Animator animator;
 
-
-    private void Awake()
-    {
-        //animator = GetComponent<Animator>();
-    }
+    private CharacterController _characterController;
 
     private void Start()
     {
+        //animator = GetComponent<Animator>();
         _characterController = GetComponent<CharacterController>();
 
         Cursor.lockState = CursorLockMode.Locked;
         // Cursor.visible = false;
-        
+    }
+    private void Update()
+    {
+        Move();
+        Jump();
+        Gravity();
     }
     
-    private void GroundedCheck()
-    {
-        // set sphere position, with offset
-        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset,
-            transform.position.z);
-        Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
-            QueryTriggerInteraction.Ignore);
-    }
-
     public void OnMove(InputAction.CallbackContext context)
     {
         inputMove = context.ReadValue<Vector2>();
     }
-
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            inputJump = context.ReadValueAsButton();
+        }
+        return;
+    }
     public void OnSprint(InputAction.CallbackContext context)
     {
         inputSprint = context.ReadValueAsButton();
     }
 
-    // public void Jump()
-    // {
-    //     //moveVector.y = jumpHeight;
-    // }
-
-    // public void OnJump(InputAction.CallbackContext context)
-    // {
-    //     if (context.performed)
-    //     {
-    //         Debug.Log("Jump pressed");
-    //         Jump();
-    //     }
-    //
-    //     return;
-    // }
-
-    private void Update()
-    {
-        Move();
-        JumpAndGravity();
-     }
-
     private void Move()
     {
-      
         // normalise input direction
-        Velocity = new Vector3(inputMove.x, 0f, inputMove.y).normalized;
+        Vector3 Velocity = new Vector3(inputMove.x, 0f, inputMove.y).normalized;
         
         if (Velocity.magnitude >= 0.1f)
         {
@@ -126,68 +85,51 @@ public class PlayerInputController : MonoBehaviour
         if (inputMove != Vector2.zero) // (Velocity.magnitude >= 0.1f && _characterController.isGrounded)
         {
             // rotation from 0 to input + camera angle
-            targetRotation = Mathf.Atan2(Velocity.x, Velocity.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            targetRotation = Mathf.Atan2(Velocity.x, Velocity.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
             // rotation smooth
             float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity,
                 rotationSmoothTime);
             
             // rotate to face input direction relative to camera position
             transform.rotation = Quaternion.Euler(0f, rotation, 0f);
-       }
+        }
         //transform rotation to movement
         Vector3 targetDirection = Quaternion.Euler(0f, targetRotation, 0f) * (Vector3.forward);
 
         // convert vel to displacement and Move the character:
         _characterController.Move(targetDirection.normalized * (movementSpeed * Time.deltaTime) + new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
-      
-        Debug.Log("On the ground");
     }
-
-    private void JumpAndGravity()
+    private void Jump()
     {
-        if (Grounded)
+        if (_characterController.isGrounded)
         {
+            if (inputJump == true)
+            {
+                //Debug.Log("jumping");
+                // the square root of H * -2 * G = how much velocity needed to reach desired height
+                verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+            }
+        }
+        else
+        {
+            Debug.Log("floating");
+            // if we are not grounded, do not jump
+            inputJump = false;
+        }
+    }
+    private void Gravity()
+    {
+        if (_characterController.isGrounded)
+        {
+            //Debug.Log("grounded");
             // stop our velocity dropping infinitely when grounded
             if (verticalVelocity < 0.0f)
             {
                 verticalVelocity = -2f;
             }
-            
-            // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-            if (verticalVelocity < _terminalVelocity)
-            {
-                verticalVelocity += gravityValue;
-            }
         }
-        else
-        {
-            // reset the jump timeout timer
-            // _jumpTimeoutDelta = JumpTimeout;
-
-            // fall timeout
-            // if (_fallTimeoutDelta >= 0.0f)
-            // {
-            //     _fallTimeoutDelta -= Time.deltaTime;
-            // }
-            // else
-            // {
-            //     // // update animator if using character
-            //     // if (_hasAnimator)
-            //     // {
-            //     //     _animator.SetBool(_animIDFreeFall, true);
-            //     // }
-            // }
-
-            // // if we are not grounded, do not jump
-            // _input.jump = false;
-        }
-        
         // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-        if (verticalVelocity < _terminalVelocity)
-        {
-            verticalVelocity += gravityValue * Time.deltaTime;
-        }
-
+        verticalVelocity += gravityValue * Time.deltaTime;
     }
 }
             
